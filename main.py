fae36d49265864bd9ec846c4be6b8b05550f0bf9
pshemach <pasindusandakan@gmail.com>
2025-10-14 12:10:36 +0530

    add packages

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from tms-backend!")
+from src.logger import logging
+from src.exception import TMSException
+import sys
 
+def main():
+    try:
+        logging.info("Hello from tms-backend!")
+        
+    except Exception as e:
+        logging.error(f"Error occurred {e}")
+        raise TMSException(e, sys)
 
 if __name__ == "__main__":
     main()
